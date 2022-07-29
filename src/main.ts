76a7dad32d25f581801ae87a0602c1c90c8f9764
Yiang <yiangforwork@gmail.com>
2022-07-29 20:58:45 +0800

    add: vim status bar

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import "uno.css";
 import * as monaco from "monaco-editor";
 import { initVimMode } from "monaco-vim";
 
-const editorDOM = document.getElementById("editor");
+const editorDOM = document.getElementById("editor") as HTMLElement;
+const vimStatus = document.getElementById("vim-status") as HTMLElement;
 
-if( editorDOM instanceof HTMLElement ) {
-    const editor = monaco.editor.create( editorDOM, {
-        value: "<?php\n\n",
-        language: "php",
-    });
+const editor = monaco.editor.create( editorDOM, {
+    value: "<?php\n\n",
+    language: "php",
+});
 
-    initVimMode( editor );
-}
+initVimMode( editor, vimStatus );
