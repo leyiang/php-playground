528000ce8db65a47a476ce2c1242efbf4b30972c
Leyiang <yiangforwork@gmail.com>
2022-08-04 19:39:30 +0800

    update: utilize webworker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
-import Unocss from "unocss/vite"; 
+import Unocss from "unocss/vite";
+import MonacoPlugin from "vite-plugin-monaco-editor";
 
 export default defineConfig({
     plugins: [
         Unocss(),
+        //@ts-ignore Weird Vite Import Bug
+        MonacoPlugin.default({})
     ]
 });
