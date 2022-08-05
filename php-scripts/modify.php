0a6788d9036fc9ce2b6494115df6a51433e04398
Leyiang <yiangforwork@gmail.com>
2022-08-05 10:05:08 +0800

    add: composer (var_dumper)

--- php-scripts/modify.php
+++ php-scripts/modify.php
@@ -4,11 +4,14 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json; charset=utf-8");
 
+function wrap( $content ) {
+    return "<?php require __DIR__ . '/vendor/autoload.php' ?>" . PHP_EOL . $content;
+}
 
 if( $_SERVER["REQUEST_METHOD"] === "POST" ) {
     $data = json_decode( file_get_contents("php://input"), true );
     $handle = fopen("./file.php", "w");
-    fwrite($handle, $data["value"]);
+    fwrite($handle, wrap($data["value"]));
 
     echo json_encode([
         "msg" => "success"
